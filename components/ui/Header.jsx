9f5bf88b902f7c344b824bbe70359578c4892564
leo <leopoldo.miranda18@tectijuana.edu.mx>
2023-05-26 00:23:00 -0700

    added: darkMode button

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -1,30 +1,49 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
-import { Layout, Badge, theme, Typography } from "antd"
+import { Layout, Badge, theme, Typography, Button } from "antd"
 const { Title } = Typography;
 
 const { Header } = Layout;
-import { IconChefHat, IconShoppingCart } from '@tabler/icons-react';
+import { IconChefHat, IconShoppingCart, IconMoonFilled, IconSunFilled } from '@tabler/icons-react';
 
 export const HeaderComponent = () => {
+  const [ isDark, setIsDark] = useState(false)
+
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
+  const handleDarkMode = () => {
+    setIsDark(!isDark)
+  }
+
   return (
     <Header style={{ padding: '0px 20px', background: colorBgContainer,display: 'flex', }}>
     <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between'}}>
+
 
     <Link href="/" style={{display: 'flex',alignItems: 'center'}}>
       <IconChefHat size={36} color='#BE3455'/>      
       <Title level={4} style={{marginLeft:'5px'}}>Magenta kitchen</Title>
     </Link>
 
-    <Link href='/checkout' style={{display: 'flex',alignItems: 'center'}}>
-      <Badge count={1}>
-        <IconShoppingCart />
-      </Badge>
-    </Link>
+      <div  style={{display: "flex"}} >
+        <div style={{display: 'flex',alignItems: 'center', marginRight: '20px'}}>
+          <Button 
+            icon={ isDark ? <IconSunFilled /> :<IconMoonFilled />} 
+            type='text' 
+            shape='circle' 
+            onClick={handleDarkMode}
+          />
+        </div>
+
+      <Link href='/checkout' style={{display: 'flex',alignItems: 'center'}}>
+        <Badge count={1}>
+          <IconShoppingCart />
+        </Badge>
+      </Link>
+      </div>
     </div>
   </Header>
   )
