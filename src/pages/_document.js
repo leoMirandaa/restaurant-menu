5ee88be562aaaccef0568a98946cbae7fde0f894
leo <leopoldo.miranda18@tectijuana.edu.mx>
2024-02-23 23:42:15 -0800

    fix: update magenta kitchen icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,32 +1,34 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="shortcut icon"
-          href="/chef-icon.ico"
-        />
+const MyDocument = () => (
+  <Html lang="en">
+    <Head>
+      <link
+        rel="shortcut icon"
+        href="/magenta-icon.ico"
+      />
 
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bitter:wght@300;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+      <link
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+      />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bitter:wght@300;500;700;900&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default MyDocument;
