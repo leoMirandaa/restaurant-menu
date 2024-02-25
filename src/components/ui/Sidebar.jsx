59d72259b81a969ecfb308ef6877b8503e520526
leo <leopoldo.miranda18@tectijuana.edu.mx>
2024-02-24 23:29:07 -0800

    fix: update Sidebar

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -1,43 +1,43 @@
-import { Layout, Menu, Typography, theme } from "antd"
+import { Layout, Menu, Typography, theme } from "antd";
 const { Sider } = Layout;
 const { Title } = Typography;
 
 import { menuOptions } from "@/utils/menuOptions";
-import styles from '../../styles/sidebar.module.css'
+import styles from "../../styles/sidebar.module.css";
 
 export const Sidebar = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
-  
+
   const { useToken } = theme;
   const { token } = useToken();
 
   return (
-    <Sider 
+    <Sider
       className={styles.sidebarMenuContainer}
-      theme='light'
-      style={{position: 'sticky'}}
+      theme="light"
+      style={{ position: "sticky" }}
       width={250}
     >
       <div className={styles.sidebarTitle}>
-        <Title 
-          level={2} 
-          style={{  
+        <Title
+          level={2}
+          style={{
             color: token.colorPrimary,
-              marginTop: '1rem',
+            marginTop: "1rem",
           }}
         >
-           Menu 
+          Menu
         </Title>
       </div>
-      
+
       <Menu
         className={styles.menuContainer}
         mode="inline"
-        defaultSelectedKeys={['starter']}
-        items= {...menuOptions}
+        defaultSelectedKeys={["starter"]}
+        items={[...menuOptions]}
       />
     </Sider>
-  )
-}
+  );
+};
